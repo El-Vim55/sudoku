a61e842b6208ebfbfaa66ab7454defe445d4d62d
El-Vim55 <abdullahnadeem62599@gmail.com>
2023-12-10 19:54:25 +1100

    Change code

--- src/main.py
+++ src/main.py
@@ -3,7 +3,14 @@
 2. initialize the starting numbers in the grid - DONE
 3. create sudoku game logic - Ongoing
     - Create User interaction with keyboard or mouse interaction -
-    - Allow user to edit open boxes and highlight selected boxes - <-
+    - Allow user to edit open boxes and highlight selected boxes - <- XXXX
+
+
+1. create 9x9 box sprites in pygame - DONE
+   - test sprite by allowing highlight feature - DONE
+2. initialize starting nums - DONE
+3. Allow editing of these nums - DONE
+4. Allow re-editing - <-
 """
 
 # Libraries
@@ -14,6 +21,7 @@ from random import randint
 # Constants
 WHITE = 0xFFFFFF
 BLACK = 0x000000
+GREY  = 0xeaeef4
 LBLUE = 0x325aaf
 
 # Setup
@@ -21,7 +29,6 @@ pygame.init()
 pygame.display.set_caption('Sudoku??? ¯\_(ツ)_/¯')
 window_surface = pygame.display.set_mode((750,850))
 window_surface.fill(WHITE)
-font = pygame.font.SysFont(None, 80)
 
 is_running = True
 
@@ -39,23 +46,40 @@ initial_nums = [
 
 
 class SudokuGrid(pygame.sprite.Sprite):
-    def __init__(self, value, row, col, sizeX, sizeY):
+    def __init__(self, value, row, col, sizeX, sizeY, font):
         super().__init__()
         self.value = value
         self.row = row
         self.col = col
         self.sizeX = sizeX
         self.sizeY = sizeY
-        # self.highlight = pygame.draw.rect(window_surface, LBLUE, size, size)
-        self.edited = False
+        self.font = font
         self.selected = False
-        
+        self.editing = False
+        self.image = pygame.Surface((80, 80))
+        self.rect = self.image.get_rect(topleft=(col*80, row*80))
+
 
     def update(self):
         if self.selected:
-            pass
-            
+            highlight_rect = pygame.Rect(((self.sizeX // 9)-5, (self.sizeY // 9)-5), (74,74))
+            self.image.fill(LBLUE, highlight_rect)
+        else:
+            self.image.fill(WHITE)
 
+        i = 1
+        while (i*80) < 720:
+            if (i*80) % 3 == 0:
+                pygame.draw.rect(self.image, BLACK, self.image.get_rect(), 3)
+            else:
+                pygame.draw.rect(self.image, BLACK, self.image.get_rect(), 1)
+            i+=1
+
+        if self.value != 0:
+            text = self.font.render(str(self.value), True, BLACK)
+            highlight_rect = pygame.Rect(((self.sizeX // 9)-5, (self.sizeY // 9)-5), (74,74))
+            self.image.fill(GREY, highlight_rect)
+            self.image.blit(text, ((self.sizeX // 3) -1, (self.sizeY // 3) -7))
 
 
 def difficulty():
@@ -65,89 +89,79 @@ def create_random_nums() -> list[list]:
     pass
 
 
-def create_grid() -> pygame.sprite:
+def create_grid_sprite() -> pygame.sprite:
     sprites = pygame.sprite.Group()
-    pygame.draw.rect(window_surface, BLACK, pygame.Rect(15, 15, 720, 720), 3)
-    
-    i = 1
-    while(i*80) < 720:
-        if (i * 80) % 3 == 0:
-            cell = pygame.draw.line(window_surface, BLACK, pygame.Vector2((i * 80) + 15, 15), pygame.Vector2((i * 80) + 15, 734), 3)
-            cell = pygame.draw.line(window_surface, BLACK, pygame.Vector2(15, (i * 80) + 15), pygame.Vector2(734, (i * 80) +15), 3)
-        else:
-            cell = pygame.draw.line(window_surface, BLACK, pygame.Vector2((i * 80) + 15, 15), pygame.Vector2((i * 80) + 15, 734), 1)
-            cell = pygame.draw.line(window_surface, BLACK, pygame.Vector2(15, (i * 80) + 15), pygame.Vector2(734, (i * 80) +15), 1)
-        sprites.add(cell)
-        i+=1
-
-def initialize_nums(nums):
-    for row in range(9):
-        for col in range(9):
-            output = nums[row][col]
-            num_text = font.render(str(output), True, BLACK)
-            window_surface.blit(num_text, pygame.Vector2((row * 80) + 40, (col * 80) + 30))
-
-def create_game_logic(nums):
-    for row in range(9):
-        for col in range(9):
-            if nums[col][row] != 45:
-                pass
-
-# create_game_logic(initial_nums)
+    for i in range(9):
+        for j in range(9):
+            cell = SudokuGrid(initial_nums[i][j], i, j, 80, 80, font)
+            sprites.add(cell)
+    return sprites
 
 
-# def check_board_logic():
-#     for i in range(9):
-#         for j in range(9):
-#             pass
-
-def user_interaction(grid):
-    create_grid()
-
-    highlight = False
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                x = x//16
-                y = y//16
-                
-                # pygame.draw.rect(window_surface, LBLUE, (x*16, y*16, 32, 32), 5)
-                # highlight = Trueb
-                
-
-
-        if highlight:
-            pass
-        
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key in range(pygame.K_1, pygame.K_9 + 1):
-        #         number = event.key - pygame.K_0
-
-
-        pygame.display.update()
-        # else:
-            # clicked = False
-
-        # if event.type == pygame.KEYDOWN:
-        #     pass
-        # if event.type == pygame.KEYUP:
-        #     moveY -= 1
-        # if event.type == pygame.K_LEFT:
-        #     moveX -= 1
-        # if event.type == pygame.K_RIGHT:
-        #     moveX += 1
+def get_clicked_cell(mouse_x, mouse_y):
+    row = mouse_y // (720 // 9)
+    col = mouse_x // (720 // 9)
+    return row, col
 
 
 if __name__ == '__main__':
+
+    font = pygame.font.SysFont(None, 80)
+    grid_sprite = create_grid_sprite()
+
+    clock = pygame.time.Clock()
+
+
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                selected_row, selected_col = get_clicked_cell(mouse_x, mouse_y)
+
+                # Update cell selection and editing
+                for cell in grid_sprite:
+                    cell.selected = (cell.row == selected_row and cell.col == selected_col)
+                    cell.editing = cell.selected and cell.value == 0
+
+            elif event.type == pygame.KEYDOWN:
+                if any(cell.editing for cell in grid_sprite):
+                    key = event.unicode
+                    # Update cell value in editing mode
+                    if key.isdigit() and 1 <= int(key) <= 9:
+                        for cell in grid_sprite:
+                            if cell.editing:
+                                cell.value = int(key)
+                                cell.editing = True
+
+        # Refresh the screen
+        grid_sprite.update()
+        sprites_to_blit = [(sprite.image, sprite.rect) for sprite in grid_sprite]
+        window_surface.blits(sprites_to_blit)
+        pygame.display.flip()
         
+    
+        
+        
+        
+b
         # drivers
+        # create_grid_sprite()
         # create_grid()
-        initialize_nums(initial_nums)
-        user_interaction(initial_nums)
+        # initialize_nums(initial_nums)
+        # user_interaction(initial_nums)
 
         # display
-        pygame.display.update()
+        # pygame.display.update()
+    # pygame.draw.rect(window_surface, BLACK, pygame.Rect(15, 15, 720, 720), 3)
+    # i = 1
+    # while(i*80) < 720:
+    #     if (i * 80) % 3 == 0:
+    #         pygame.draw.line(window_surface, BLACK, pygame.Vector2((i * 80) + 15, 15), pygame.Vector2((i * 80) + 15, 734), 3)
+    #         pygame.draw.line(window_surface, BLACK, pygame.Vector2(15, (i * 80) + 15), pygame.Vector2(734, (i * 80) +15), 3)
+    #     else:
+    #         pygame.draw.line(window_surface, BLACK, pygame.Vector2((i * 80) + 15, 15), pygame.Vector2((i * 80) + 15, 734), 1)
+    #         pygame.draw.line(window_surface, BLACK, pygame.Vector2(15, (i * 80) + 15), pygame.Vector2(734, (i * 80) +15), 1)
+    #     i+=1
